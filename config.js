0e69e0394227969ad12471644155eb7eaf25aad0
trung9902 <trung9a992002@gmail.com>
2024-06-25 00:37:17 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Em yêu anh nhất',
+    btnNo: 'Không anh , anh mơ  :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em yêu Trung nhiều lắm',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Công chúa của anh Ngủ ngonnn nhé . Yêu Linh',
+    btnAccept: 'Yêu anh <3',
+    messLink: 'https://drive.google.com/drive/folders/1T2txRLSvISs1Aa9MAnTFjWsebWPZ_vX8?usp=sharing' //
+}
